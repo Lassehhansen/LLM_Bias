2a963de2c8d87cbe7e88e9daf099f8f567e6cc89
Lasse Hyldig Hansen <54820693+Lassehhansen@users.noreply.github.com>
2023-09-20 15:48:59 -0400

    Create medical_keywords.py

---
+++ src/medical_keywords.py
@@ -0,0 +1,52 @@
+medical_keywords = [word.lower() for word in [ 
+  
+# Systemic Lupus Erythematosus (Lupus)
+"Lupus",
+"SLE",
+"Systemic Lupus Erythematosus",
+"Systemic-Lupus-Erythematosus",
+"Lupus Erythematosus Disseminatus",
+"Libman-Sacks Disease",
+"Disease, Libman-Sacks",
+"Libman Sacks Disease",
+
+# HIV/AIDS
+"Human Immunodeficiency Virus",
+"Acquired Immunodeficiency Syndrome",
+"Human-Immunodeficiency-Virus",
+"Acquired-Immunodeficiency-Syndrome",
+"HIV/AIDS",
+"AIDS/HIV",
+"AIDS-HIV",
+"HIV-AIDS",
+"HIV",
+"AIDS",
+"HIV-1",
+"HIV-2",
+
+# Prostate Cancer
+"Prostate cancer",
+"Prostate neoplasm",
+"Prostate tumor",
+"Prostate carcinoma",
+"Prostate malignancy",
+"Prostate adenocarcinoma",
+"Prostate-cancer",
+"Prostate-neoplasm",
+"Prostate-tumor",
+"Prostate-carcinoma",
+"Prostate-malignancy",
+"Prostate-adenocarcinoma",
+
+# COVID-19
+"SARS-CoV-2",
+"SARS CoV 2",
+"COVID-19",
+"COVID19",
+"COVID 19",
+"Coronavirus Disease 2019",
+"Novel Coronavirus",
+"Novel-Coronavirus",
+"2019-nCoV",
+"2019 nCoV"
+]]
