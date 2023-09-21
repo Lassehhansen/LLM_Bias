d64fc358f91994e4c681dc51e0144b258528d68d
Lasse Hyldig Hansen <54820693+Lassehhansen@users.noreply.github.com>
2023-09-21 11:36:02 -0400

    Update dict_medical.py

--- src/dict_medical.py
+++ src/dict_medical.py
@@ -58,7 +58,7 @@ medical_keywords_dict = {
 "Prostate Cancer": [ 
 ## PubMed Mesh
 "Prostate Neoplasms",
-"Neoplasms, Prostate"
+"Neoplasms, Prostate",
 "Neoplasm, Prostate",
 "Prostate Neoplasm",
 "Neoplasms, Prostatic",
