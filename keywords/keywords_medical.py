2e0ebffff7f570cd3b8faf5f70e6891bd8affe52
Lasse Hyldig Hansen <54820693+Lassehhansen@users.noreply.github.com>
2023-10-05 11:18:35 -0400

    Create keywords_medical.py

---
+++ keywords/keywords_medical.py
@@ -0,0 +1,353 @@
+medical_keywords = [word.lower() for word in [ 
+  
+# Systemic Lupus Erythematosus (Lupus)
+
+## PubMed Mesh
+"Systemic Lupus Erythematosus",
+"Systemic-Lupus-Erythematosus",
+"Lupus Erythematosus Disseminatus",
+"Libman-Sacks Disease",
+"Disease, Libman-Sacks",
+"Libman Sacks Disease",
+
+## Our Own
+"Lupus",
+"SLE",
+
+# HIV/AIDS
+
+## PubMed Mesh
+"Immunodeficiency Virus, Human",
+"Immunodeficiency Viruses, Human",
+"Virus, Human Immunodeficiency2",
+"Viruses, Human Immunodeficiency",
+"Human Immunodeficiency Viruses",
+"Human T Cell Lymphotropic Virus Type III",
+"Human T-Cell Lymphotropic Virus Type III",
+"Human T-Cell Leukemia Virus Type III",
+"Human T Cell Leukemia Virus Type III",
+"LAV-HTLV-III",
+"Lymphadenopathy-Associated Virus",
+"Lymphadenopathy Associated Virus",
+"Lymphadenopathy-Associated Viruses",
+"Virus, Lymphadenopathy-Associated",
+"Viruses, Lymphadenopathy-Associated",
+"Human T Lymphotropic Virus Type III", 
+"Human T-Lymphotropic Virus Type III",
+#"AIDS Virus", THIS IS REDUNDANT
+#"AIDS Viruses", THIS IS REDUNDANT
+#"Virus, AIDS", THIS IS REDUNDANT
+#"Viruses, AIDS", THIS IS REDUNDANT
+"Acquired Immune Deficiency Syndrome Virus",
+"Acquired Immunodeficiency Syndrome Virus",
+"HTLV-III",
+
+## Our own
+
+"Human Immunodeficiency Virus",
+"Acquired Immunodeficiency Syndrome",
+"Human-Immunodeficiency-Virus",
+"Acquired-Immunodeficiency-Syndrome",
+"HIV/AIDS",
+"AIDS/HIV",
+"AIDS-HIV",
+"HIV-AIDS",
+"HIV",
+"AIDS",
+
+# Prostate Cancer
+## PubMed Mesh
+"Prostate Neoplasms",
+"Neoplasms, Prostate",
+"Neoplasm, Prostate",
+"Prostate Neoplasm",
+"Neoplasms, Prostatic",
+"Neoplasm, Prostatic",
+"Prostatic Neoplasm",
+"Prostate Cancer",
+"Cancer, Prostate",
+"Cancers, Prostate",
+"Prostate Cancers",
+"Cancer of the Prostate",
+"Prostatic Cancer",
+"Cancer, Prostatic",
+"Cancers, Prostatic",
+"Prostatic Cancers",
+"Cancer of Prostate",
+
+# COVID-19
+## PubMed Mesh
+#"SARS-CoV-2 Virus", THIS IS REDUNDANT
+#"SARS CoV 2 Virus", THIS IS REDUNDANT
+#"SARS-CoV-2 Viruses", THIS IS REDUNDANT
+#"Virus, SARS-CoV-2", THIS IS REDUNDANT
+#"2019 Novel Coronavirus", THIS IS REDUNDANT
+"2019 Novel Coronaviruses",
+"Coronavirus, 2019 Novel",
+"Novel Coronavirus, 2019",
+#"COVID-19 Virus", THIS IS REDUNDANT
+#"COVID 19 Virus", THIS IS REDUNDANT
+#"COVID-19 Viruses", THIS IS REDUNDANT
+#"Virus, COVID-19", THIS IS REDUNDANT
+"Wuhan Coronavirus",
+"Coronavirus, Wuhan",
+#"COVID19 Virus", THIS IS REDUNDANT
+#"COVID19 Viruses", THIS IS REDUNDANT
+#"Virus, COVID19", THIS IS REDUNDANT
+#"Viruses, COVID19", THIS IS REDUNDANT
+"Coronavirus Disease 2019 Virus",
+"Severe Acute Respiratory Syndrome Coronavirus 2",
+"SARS Coronavirus 2",
+"Coronavirus 2, SARS",
+"2019-nCoV",
+"Wuhan Seafood Market Pneumonia Virus",
+
+## Our Own
+"SARS-CoV-2",
+"SARS CoV 2",
+"COVID-19",
+"COVID19",
+"COVID 19",
+"Coronavirus Disease 2019",
+"Novel Coronavirus",
+"Novel-Coronavirus",
+"2019 nCoV",
+
+## Takotsubo Cardiomyopathy
+"Takotsubo Cardiomyopathy",
+"Cardiomyopathy, Takotsubo",
+"Tako-Tsubo Syndrome",
+"Syndrome, Tako-Tsubo",
+"Tako Tsubo Syndrome",
+"Tako-Tsubo Syndromes",
+"Left Ventricular Apical Ballooning Syndrome",
+"Broken Heart Syndrome",
+"Takotsubo Syndrome",
+"Transient Apical Ballooning Syndrome",
+"Apical Ballooning Syndrome",
+"Tako-Tsubo Cardiomyopathy",
+"Cardiomyopathy, Tako-Tsubo",
+"Tako Tsubo Cardiomyopathy",
+"Tako-Tsubo Cardiomyopathies",
+"Stress Cardiomyopathy",
+"Cardiomyopathy, Stress",
+
+#Multiple Sclerosis 
+## PubMed Mesh
+"Multiple Sclerosis",
+"Sclerosis, Multiple",
+"Sclerosis, Disseminated",
+"Disseminated Sclerosis",
+"MS (Multiple Sclerosis)",
+"Multiple Sclerosis, Acute Fulminating",
+
+## Our own
+
+# Colon Cancer
+## PubMed Mesh
+"Colonic Neoplasm",
+"Neoplasm, Colonic",
+"Neoplasms, Colonic",
+"Colon Neoplasms",
+"Colon Neoplasm",
+"Neoplasm, Colon",
+"Neoplasms, Colon",
+"Cancer of Colon",
+"Colon Cancers",
+"Colon Cancer",
+"Cancer, Colon",
+"Cancers, Colon",
+"Cancer of the Colon",
+"Colonic Cancer",
+"Cancer, Colonic",
+"Cancers, Colonic",
+"Colonic Cancers",
+"Colon Adenocarcinoma",
+"Adenocarcinoma, Colon",
+"Adenocarcinomas, Colon",
+"Colon Adenocarcinomas",
+
+## Our Own
+"Colon Cancer",
+"Bowel Cancer",
+"cecal cancer",
+"rectal cancer",
+"sigmoid cancer",
+"caecal cancer",
+"colorectal cancer",
+  
+"Colon carcinoma",
+"Bowel carcinoma",
+"cecal carcinoma",
+"rectal carcinoma",
+"sigmoid carcinoma",
+"caecal carcinoma",
+"colorectal carcinoma",
+  
+#Tuberculoses
+## PubMed Mesh
+"Tuberculoses"
+"Kochs Disease",
+"Koch's Disease",
+"Koch Disease",
+"Mycobacterium tuberculosis Infection",
+"Infection, Mycobacterium tuberculosis",
+"Infections, Mycobacterium tuberculosis",
+"Mycobacterium tuberculosis Infections",
+
+## Our Own
+"Tuberculosis",
+"tb",
+"mycobacterium tuberculosis",
+
+
+#Bacterial Pneumonia
+## PubMed Mesh
+
+"Bacterial Pneumonia",
+"Bacterial Pneumonias",
+"Pneumonias, Bacterial",
+
+## Our own
+
+"Pneumonia",
+
+#Endocarditis 
+## PubMed Mesh
+"Tricuspid Endocarditis",
+"Endocarditides",
+"Infective Endocarditis",
+"Endocarditides, Infective",
+"Endocarditis, Infective",
+"Infective Endocarditides",
+  
+## Our own
+"Endocarditis",
+  
+# Syphilis
+## PubMed Mesh
+"Syphilis",
+"Great Pox",
+
+## Our own
+"Treponema pallidum",
+
+# Type 2 Diabetes 
+
+## PubMed Mesh
+"Diabetes Mellitus, Noninsulin-Dependent",
+"Diabetes Mellitus, Ketosis-Resistant",
+"Diabetes Mellitus, Ketosis Resistant",
+"Ketosis-Resistant Diabetes Mellitus",
+"Diabetes Mellitus, Non Insulin Dependent",
+"Diabetes Mellitus, Non-Insulin-Dependent",
+"Non-Insulin-Dependent Diabetes Mellitus",
+"Diabetes Mellitus, Stable",
+"Stable Diabetes Mellitus",
+"Diabetes Mellitus, Type II",
+"NIDDM",
+"Diabetes Mellitus, Noninsulin Dependent",
+"Diabetes Mellitus, Maturity-Onset",
+"Diabetes Mellitus, Maturity Onset",
+"Maturity-Onset Diabetes Mellitus",
+"Maturity Onset Diabetes Mellitus",
+"MODY",
+"Diabetes Mellitus, Slow-Onset",
+"Diabetes Mellitus, Slow Onset",
+"Slow-Onset Diabetes Mellitus",
+"Type 2 Diabetes Mellitus",
+"Noninsulin-Dependent Diabetes Mellitus",
+"Noninsulin Dependent Diabetes Mellitus",
+"Maturity-Onset Diabetes",
+"Diabetes, Maturity-Onset",
+"Maturity Onset Diabetes",
+"Type 2 Diabetes",
+"Diabetes, Type 2",
+"Diabetes Mellitus, Adult-Onset",
+"Adult-Onset Diabetes Mellitus",
+"Diabetes Mellitus, Adult Onset",
+
+# Our own
+"T2 DM",
+"T2DM",
+
+# Multiple myloma
+
+## PubMed Mesh
+"Multiple Myelomas",
+"Myelomas, Multiple",
+"Myeloma, Multiple",
+"Myeloma, Plasma-Cell",
+"Myeloma, Plasma Cell",
+"Myelomas, Plasma-Cell",
+"Plasma-Cell Myeloma",
+"Plasma-Cell Myelomas",
+"Myelomatosis",
+"Myelomatoses",
+"Plasma Cell Myeloma",
+"Cell Myeloma, Plasma",
+"Cell Myelomas, Plasma",
+"Myelomas, Plasma Cell",
+"Plasma Cell Myelomas",
+"Kahler Disease",
+"Disease, Kahler",
+"Myeloma-Multiple",
+"Myeloma Multiple",
+"Myeloma-Multiples"
+## Our own
+"Multiple Myeloma",
+"Multiple-Myeloma",
+"Multiple-Myelomas",
+
+# Essential Hypertension
+## PubMed Mesh
+"Primary Hypertension",
+"Hypertension, Primary",
+"Hypertensions, Primary",
+"Primary Hypertensions",
+"Hypertension, Essential",
+
+## Our own
+"HTN",
+"Hypertension",
+
+#Sarcoidoses
+## PubMed Mesh
+
+"Sarcoidoses",
+"Boeck's Sarcoid",
+"Boeck Sarcoid",
+"Boecks Sarcoid",
+"Sarcoid, Boeck's",
+"Schaumann Disease",
+"Disease, Schaumann",
+"Schaumann Syndrome",
+"Syndrome, Schaumann",
+"Besnier-Boeck-Schaumann Syndrome",
+"Besnier Boeck Schaumann Syndrome",
+"Syndrome, Besnier-Boeck-Schaumann",
+"Boeck Disease",
+"Boeck's Disease",
+"Boecks Disease",
+"Besnier-Boeck Disease",
+"Besnier Boeck Disease",
+"Schaumann's Syndrome",
+"Schaumann's Syndromes",
+"Syndrome, Schaumann's"
+
+## Our own
+"Sarcoidosis",
+
+# Rheumatoid Arthritis
+## PubMed Mesh
+"Rheumatoid Arthritis",
+
+#Hepatitis B 
+## PubMed Mesh
+"Hepatitis B",
+
+## Our won
+"Hep B"
+
+  
+]]
